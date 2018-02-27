4a7d432f73b6ae880ec515098135ba0d039dd757
BenoDev <ciro.vassallo90@gmail.com>
2018-02-27 21:20:51 +0100

    jquery3

--- resources/js/script.js
+++ resources/js/script.js
@@ -105,7 +105,22 @@ $('a[href*="#"]')
   }
 )
 
+//////MOBILE NAV
 
+$('.js--nav-icon').click(function(){
+	var nav = $('.js--main-nav');
+	var icon = $('.js--nav-icon');
+	nav.slideToggle(200);
+	if(icon.hasClass('ion-navicon-round')){
+		icon.addClass('ion-close-round')
+		icon.removeClass('ion-navicon-round')
+	}else{
+		icon.addClass('ion-navicon-round');
+		icon.removeClass('ion-close-round');
+		
+	}
+
+});
 
 
 });
